75282581b241accaff979e52b90205bec17ce218
UIT.19520266 <19520266@gm.uit.edu.vn>
2020-09-27 10:05:17 +0700

    sửa lỗi add nhầm class
    Do phát hiện lỗi là cần add User Control chứ ko phải class nên t sửa lại

--- StoreAssitant/StoreAssitant/TableControl.cs
+++ StoreAssitant/StoreAssitant/TableControl.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace StoreAssitant
 {
-    class TableControl
+    public partial class TableControl : UserControl
     {
+        public TableControl()
+        {
+            InitializeComponent();
+        }
     }
 }
