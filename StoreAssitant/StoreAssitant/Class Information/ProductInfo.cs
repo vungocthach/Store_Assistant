058bf2916f289853a9e7a6e683100101f0436784
Dragnell <bang2001vl@outlook.com.vn>
2020-10-06 08:47:27 +0700

    Configuaration for tableView
    Add 2 class
    + TableInfo.cs
    + ProductInfo.cs

---
+++ StoreAssitant/StoreAssitant/Class Information/ProductInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreAssitant
+{
+    public class ProductInfo
+    {
+        public Image Image { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string[] Tags { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder buider = new StringBuilder();
+            if (Image == null) { buider.Append("Image : null").AppendLine(); }
+            else { buider.Append("Image : ").Append(Image.ToString()).AppendLine(); }
+            buider.Append("Name : ").Append(Name).AppendLine();
+            buider.Append("Price : ").Append(Price).AppendLine();
+            buider.Append("Tags : ");
+            foreach (string str in Tags)
+            {
+                buider.AppendLine(str);
+            }
+            buider.Append("Description : ").Append(Description).AppendLine();
+            return buider.ToString();
+        }
+    }
+}
--- StoreAssitant/StoreAssitant/Form1.cs
+++ StoreAssitant/StoreAssitant/Form1.cs
@@ -13,28 +13,22 @@ namespace StoreAssitant
 {
     public partial class Form1 : Form
     {
-        ProductInfo productInfo;
         public Form1()
         {
             InitializeComponent();
             kryptonNavigator1.GotFocus += KryptonNavigator1_GotFocus;
-            btn_Test.Click += Btn_Test_Click;
-        }
-
-        private void Btn_Test_Click(object sender, EventArgs e)
-        {
-            OpenAddProductDialog();
         }
 
         private void OpenAddProductDialog()
         {
-            AddProductForm form = new AddProductForm();
-            form.ClickSubmitOK += new EventHandler<ProductInfo>((object sender, ProductInfo info) =>
+            using (AddProductForm form = new AddProductForm())
             {
-                this.productInfo = info;
-                btn_Test.Text = productInfo.ToString();
-            });
-            form.ShowDialog();
+                form.ClickSubmitOK += new EventHandler<ProductInfo>((object sender, ProductInfo info) =>
+                {
+                    // Do something with ProductInfo
+                });
+                form.ShowDialog();
+            }
         }
 
         private void KryptonNavigator1_GotFocus(object sender, EventArgs e)
