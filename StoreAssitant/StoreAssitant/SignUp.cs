3fee53d57daee3eb973d55e9f652427d56998116
UIT.19521973 <67250352+Nhuc-Huynh@users.noreply.github.com>
2020-11-29 19:42:31 +0700

    create SignUp form
    Consist of: Name, Passwork, Sex,  Phone number, Date of birth

---
+++ StoreAssitant/StoreAssitant/SignUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StoreAssitant
+{
+    public partial class SignUp : Form
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
