9ef2ca80dfbcf3947836b5a5a4921a7245476f26
Dragnell <bang2001vl@outlook.com.vn>
2020-11-29 13:35:22 +0700

    New style for statistic view

---
+++ StoreAssitant/StoreAssitant/StoreAssistant_Information/SaleInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreAssitant.StoreAssistant_Information
+{
+    public class SaleInfo
+    {
+        public DateTime DateMin;
+        public DateTime DateMax;
+
+        public void SetDay(int year, int month, int day)
+        {
+            DateMin = new DateTime(year, month, day, 0, 0, 0);
+            DateMax = new DateTime(year, month, day, 23, 59, 59);
+        }
+
+        public void SetMonth(int year, int month)
+        {
+            DateMin = new DateTime(year, month, 1, 0, 0, 0);
+            DateMax = new DateTime(year, month, DateTime.DaysInMonth(year, month), 23, 59, 59);
+        }
+
+        public void SetYear(int year)
+        {
+            DateMin = new DateTime(year, 1, 1, 0, 0, 0);
+            DateMax = new DateTime(year, 12, 31, 23, 59, 59);
+        }
+
+        public Dictionary<string, ProductSaleInfo> Products;
+
+        public object Tag;
+
+        public long GetRevenue()
+        {
+            long rs = 0;
+            foreach (KeyValuePair<string, ProductSaleInfo> p in Products) { rs += p.Value.GetRenvenue(); }
+            return rs;
+        }
+
+        public SaleInfo()
+        {
+            Products = new Dictionary<string, ProductSaleInfo>();
+            DateMax = DateTime.Now;
+            DateMin = DateMax.AddDays(-1);
+        }
+
+        public SaleInfo(int year, int month, int day)
+        {
+            Products = new Dictionary<string, ProductSaleInfo>();
+            SetDay(year, month, day);
+        }
+
+    }
+}
---
+++ StoreAssitant/StoreAssitant/StoreAssistant_StatiticsView/TimePickerForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StoreAssitant.StoreAssistant_StatiticsView
+{
+    public partial class TimePickerForm : Form
+    {
+        public DateTime DateMin { get => dtp_From.Value; set => dtp_From.Value = value; }
+        public DateTime DateMax { get => dtp_To.Value; set => dtp_To.Value = value; }
+
+        public event EventHandler ClickedSubmitOK;
+
+        public TimePickerForm()
+        {
+            InitializeComponent();
+
+            ClickedSubmitOK = new EventHandler((s, e) => { });
+
+            this.btnSubmit.Click += Button1_Click;
+            this.btnDefault.Click += BtnDefault_Click;
+        }
+
+        private void BtnDefault_Click(object sender, EventArgs e)
+        {
+            dtp_From.Value = DateTime.MinValue;
+            dtp_To.Value = DateTime.Today;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ClickedSubmitOK(this, null);
+        }
+    }
+}
