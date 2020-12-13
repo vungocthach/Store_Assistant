4cc466dd880331dbdc7c5673f725804043682160
UIT.19520266 <19520266@gm.uit.edu.vn>
2020-12-13 19:05:48 +0700

    add background FormSignUp with no transparant

--- StoreAssitant/StoreAssitant/Properties/Resources.Designer.cs
+++ StoreAssitant/StoreAssitant/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace StoreAssitant.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Background {
+            get {
+                object obj = ResourceManager.GetObject("Background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap BGSt {
             get {
                 object obj = ResourceManager.GetObject("BGSt", resourceCulture);
